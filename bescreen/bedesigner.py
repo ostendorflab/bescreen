7dec7788f3aa7cfe2ff2f17e39196484b76aa2d2
gernophil <gernophil@gmx.net>
2025-04-26 23:44:31 +0200

    fixed polars bug at concat

--- bescreen/bedesigner.py
+++ bescreen/bedesigner.py
@@ -1353,7 +1353,7 @@ def design_bes(annotation_file,
         for col in [filtercol for filtercol in sgrnas_filtered_out.columns if filtercol not in ['variant', 'base_change', 'strand', 'ref_match']]:
             sgrnas_filtered_out = sgrnas_filtered_out.with_columns(pl.lit("no_guides_found").alias(col))
 
-        sgrnas_not_or_filtered_out = pl.concat([sgrnas_not_to_filter, sgrnas_filtered_out]).unique() # there shouldn't be any duplicates, but unique() necessary for correct concat
+        sgrnas_not_or_filtered_out = pl.concat([sgrnas_not_to_filter, sgrnas_filtered_out], how="vertical_relaxed").unique() # there shouldn't be any duplicates, but unique() necessary for correct concat
         sgrnas = pl.concat([sgrnas_not_or_filtered_out, sgrnas_to_filter])
 
         sgrnas = sgrnas.group_by([col for col in sgrnas.columns if col not in (symbols_to_contract + columns_to_modify_last)], maintain_order=True).agg(pl.all()) # revert back to the original form before exploding
